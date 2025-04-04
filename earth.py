d28b21c819ea59163f37ee6093cab9593baf035c
sandersen9 <sandersen@mail.austral.edu.ar>
2025-04-04 11:19:35 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
 def earth():
-    x = "Bangladesh"
-    y = "Barbados"
+    paisy = "Barbados"
+    paisx = "Bangladesh"
+
+    resultadox = paisx<paisy
+    resultadoy = paisx>paisy
+
+    print(f"The result of {paisx} comes first in the dictionary than {paisy} is {resultadox}")
+    print(f"The result of {paisy} comes first in the dictionary than {paisx} is {resultadoy}")
